466703a5e9c2a63d81f5e26a0e7ef8de234e2f78
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-04-12 23:36:18 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import logo from './logo.svg';
-import Pokecard from './component/Pokecard';
-import pokeJaune from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import Log from './assets/pokeJaune.png'
-import './component/Pokecard.css'
+import Titre from "./component/titre";
 
 function App() {
+  fetch("restcountries.com/v3.1/all")
+  .then(Response=> Response.json())
+  .then(data=>console.log(data));
+  
   return (
-   
-    <div>
-    <div className='title'> <h2>Pokedex</h2> </div>
-    <div className='gridContainer'>
-
     
-    <Pokecard title="Charmender" image={pokeJaune} type="water" exp="63"/>
-    <Pokecard title="Squirtle" image={Log} type="water" exp="63"/>
-    <Pokecard title="" image={Log} type="water" exp="63"/>
-    <Pokecard title="Metapod" type="bug" exp="72"/>
-    <Pokecard title="Butterfree" type="flying" exp="178"/>
-    <Pokecard title="Butterfree" type="flying" exp="178"/> 
-    <Pokecard title="Butterfree" type="flying" exp="178"/> 
-    <Pokecard title="Butterfree" type="flying" exp="178"/> 
-    </div>
-    </div>
+   <div>
+     
+     <Titre />
+   </div>
+    
   );
 }
 
---
+++ src/component/titre.js
@@ -0,0 +1,9 @@
+const Titre = () => {
+    return ( 
+        <div>
+            <h1>WikiNations</h1>
+        </div>
+     );
+}
+ 
+export default Titre;
